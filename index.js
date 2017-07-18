7dc513f7b72716f89d738b9bd0bb363ecaef8abd
aksonov <aksonov@gmail.com>
2017-07-18 14:09:11 +0200

    add cube custom navigator

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { requireNativeComponent } from 'react-native';
-
+import CubeNavigator from './CubeNavigator';
 export const RNCubeTransition = requireNativeComponent('RNCubeTransition', null);
+export CubeNavigator;
